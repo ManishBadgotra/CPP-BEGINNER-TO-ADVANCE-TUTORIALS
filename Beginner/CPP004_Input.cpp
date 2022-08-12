b00b2f07d98d82c1d32cbaf451875a78e657e011
Manish Sharma <badgotra.manish@gmail.com>
2022-08-12 17:58:29 +0530

    Beginner Tutorial is started.

---
+++ Beginner/CPP004_Input.cpp
@@ -0,0 +1,22 @@
+/*
+In this session we will learn how to get input from a user.
+*/
+#include<iostream>
+using namespace std;
+
+main(){
+    // cout is used to output to a user. It means console out.
+    // cin is used to take input from a user. It means console in.
+
+   string firstname, lastname;
+    cout << "Enter your First Name: " << firstname;
+    cout << "\nEnter your Last Name: " << lastname;
+
+    //This will print a first and last name of a user that is given by him.
+    cout << "\nWelcome " << firstname << " " << lastname; 
+    // Now try to get any other type of variable as input and print it to the user.
+
+    // Look how I have added space between a first and last name, Because while output from variable space will not be included automatically. Try to run below line to get an example of what I'm saying.
+
+    // cout << "\nWelcome " << firstname << lastname;
+}
